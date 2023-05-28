e9c92d62ed6d88adf32fdcc8b1aaca36bd75dfe9
Jon Mico <jonmico@gmail.com>
2023-05-27 22:25:51 -0400

    add initial state, list the checklist

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Checklist from './components/Checklist';
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
       <Checklist />
     </div>
   );
--- src/components/Checklist.tsx
+++ src/components/Checklist.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react';
 
+interface Check {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
 export default function Checklist() {
+  const [list, setList] = useState<Check[]>([
+    { id: 'string1', text: 'walk the dog', completed: true },
+    { id: 'check2', text: 'walk the cat', completed: false },
+    { id: 'check3', text: 'make dinner', completed: false },
+  ]);
+
   return (
     <div>
-      <h2>This is the Checklist component</h2>
+      <h1>CheckList App</h1>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
